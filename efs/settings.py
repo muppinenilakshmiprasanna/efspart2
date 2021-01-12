fe3efe24d39ad5609b2ef94366b66931639f66c5
muppinenilakshmiprasanna <lpmuppineni26@gmail.com>
2021-01-12 15:02:32 -0600

    commit for part 1

--- efs/settings.py
+++ efs/settings.py
@@ -124,13 +124,6 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = '/home'
 #LOGOUT_REDIRECT_URL='/home'
 
-"""EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'djangoeaformasters@gmail.com'
-EMAIL_HOST_PASSWORD = 'django@123'
-DEFAULT_FROM_EMAIL = 'efs<noreply@efservices.com>'"""
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
